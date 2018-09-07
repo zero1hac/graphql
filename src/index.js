9e3048050fc87e9440f4065a1813e56296ddbfba
dushyant <6dushyantmishra20101@gmail.com>
2018-09-07 15:06:19 +0530

    Added database and prisma schema

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
 const { GraphQLServer } = require('graphql-yoga')
 
 // 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+
+
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
 
 // 2
+let idCount = links.length
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+
+    feed: () => links,
+  },
+
+  Mutation: {
+
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+
+      links.push(link)
+      return link
+    }
+  },
+
+  Link: {
+      id: (root) => root.id,
+      description: (root) => root.description,
+      url: (root) => root.url,
   }
 }
 
-// 3
+
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
